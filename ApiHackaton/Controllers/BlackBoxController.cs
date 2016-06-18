a5f337ced74044a9af8cd9fd9bb2ba00aa59c747
lmmoreira <lmmoreira@BRJ0-WKS-006>
2016-06-18 16:55:44 -0300

    Add project files.

---
+++ ApiHackaton/Controllers/BlackBoxController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+using ApiHackaton.ApiClient.BlackBoxApi;
+using ApiHackaton.Entities;
+
+namespace ApiHackaton.Controllers
+{
+    public class BlackBoxController : ApiController
+    {
+        public BlackBoxClientApi BlackBoxClientApi;
+
+        public BlackBoxController()
+        {
+            BlackBoxClientApi = new BlackBoxClientApi();
+        }
+        public JsonResult<List<Merchant>> GetMerchants()
+        {
+            return Json(BlackBoxClientApi.GetMerchants());
+        }
+
+        public JsonResult<List<Customer>> GetCustomers()
+        {
+            return Json(BlackBoxClientApi.Getcustomers());
+        }
+
+        public JsonResult<Customer> GetCustomer([FromUri] int id)
+        {
+            return Json(BlackBoxClientApi.Getcustomer(id));
+        }
+    }
+}
